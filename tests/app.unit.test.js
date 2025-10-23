cb7c3c5f40c0ca046c4d9a484264f4c9bb8ba124
Paul00Kevin <kpa.benalcazar@yavirac.edu.ec>
2025-10-23 19:22:49 +0200

    segundo commit: fallo de test

--- tests/app.unit.test.js
+++ tests/app.unit.test.js
@@ -1,8 +1,8 @@
 // Funcionamiento del test
 const suma = (a, b) => a + b;
 
-test('la suma de 2 + 3 debe ser 6', () => {
-  expect(suma(2, 3)).toBe(6);
+test('la suma de 2 + 3 debe ser 7', () => {
+  expect(suma(2, 3)).toBe(7);
 });
 
 test('la suma de números negativos funciona correctamente', () => {
